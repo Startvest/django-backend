0de279100e124f1c6b426c27440ba77e56f42e65
Tamunokorite Briggs <obriggs03@gmail.com>
2021-07-27 15:45:52 +0100

    Deploying

--- startvest_api/settings.py
+++ startvest_api/settings.py
@@ -177,6 +177,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
