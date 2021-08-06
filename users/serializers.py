8e1f643ef30afd82dfe3588a15b073618ef77e66
Tamunokorite Briggs <obriggs03@gmail.com>
2021-08-07 00:09:45 +0100

    Fixing registration issue

--- users/serializers.py
+++ users/serializers.py
@@ -1,16 +1,23 @@
-# from rest_framework import serializers
-# from .models import User, user_type
-# from dj_rest_auth.registration.serializers import RegisterSerializer
+from rest_framework import serializers
+from .models import User, user_type
+from dj_rest_auth.registration.serializers import RegisterSerializer
 
-# class UserSerializer(RegisterSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['email', 'password']
-#         extra_kwargs = {'password': {'write_only': True}}
+class UserSerializer(RegisterSerializer):
+    # class Meta:
+    #     model = User
+    #     fields = ['email', 'password']
+    #     extra_kwargs = {'password': {'write_only': True}}
     
 
-#     def create(self, validated_data):
-#         user = User(email=validated_data['email'])
-#         user.set_password(validated_data['password'])
-#         user.save()
-#         return user
+    # def create(self, validated_data):
+    #     user = User(email=validated_data['email'])
+    #     user.set_password(validated_data['password'])
+    #     user.save()
+    #     return user
+
+    username = None
+
+    def save(self, request):
+        user = super().save(request)
+        user.save()
+        return user
