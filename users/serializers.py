abb1df73fcd596272d61f9fa0c19ff42d8778cbd
Tamunokorite Briggs <obriggs03@gmail.com>
2021-08-06 23:00:44 +0100

    FIxing registration issue

--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import User, user_type
 
 class UserSerializer(serializers.ModelSerializer):
+    username = None
     class Meta:
         model = User
         fields = ('email', 'password')
