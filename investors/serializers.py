7ed50a8647e06bf53ec3f2b8836738b99a1a36c1
Tamunokorite Briggs <obriggs03@gmail.com>
2021-08-11 12:22:41 +0100

    Fixing Investor Model

--- investors/serializers.py
+++ investors/serializers.py
@@ -5,6 +5,7 @@ from .models import Investor, Investment
 class InvestmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Investment
+        fields = '__all__'
     
 class InvestorSerializer(serializers.ModelSerializer):
     class Meta:
