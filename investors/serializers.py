9e0b452227a2355ec3710c2ab9e825e5eb22f8a8
Tamunokorite Briggs <obriggs03@gmail.com>
2021-08-11 12:15:10 +0100

    Fixing Investor Model

--- investors/serializers.py
+++ investors/serializers.py
@@ -5,12 +5,10 @@ from .models import Investor, Investment
 class InvestmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Investment
-        fields = ['registered_business', 'interests', 'verified']
-
     
 class InvestorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Investor
-        fields = '__all__'
+        fields = ['registered_business', 'interests', 'verified']
     
     investments = InvestmentSerializer(many=True, read_only=True)
