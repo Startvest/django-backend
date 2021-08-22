30906a53afde822c33d6d3134e31d27bcd46bfb4
Tamunokorite Briggs <obriggs03@gmail.com>
2021-08-22 19:44:18 +0100

    Added get user type endpoint

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework import status
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -8,6 +9,8 @@ from django.shortcuts import get_object_or_404
 
 from .models import User, user_type
 from .serializers import UserTypeSerializer
+
+from startups.views import MultipleFieldLookupMixin
 
 # Create your views here.
 @api_view(['POST', ])
@@ -26,3 +29,8 @@ def create_user_type(request, uid):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserTypeInfo(MultipleFieldLookupMixin, generics.RetrieveAPIView):
+    queryset = user_type.objects.all()
+    serializer_class = UserTypeSerializer
+    lookup_fields = ['user_id']
