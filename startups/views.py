ecc5fa497027656c546b3a3bcd268d4503d83929
Tamunokorite Briggs <obriggs03@gmail.com>
2021-08-24 14:04:40 +0100

    Modified Permissions

--- startups/views.py
+++ startups/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework.premissions import AllowAny
+from rest_framework.permissions import AllowAny
 
 from django.shortcuts import get_object_or_404
 
