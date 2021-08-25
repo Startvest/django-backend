d8af7e265e9c15850af38bae7b2e385764f0944b
Tamunokorite Briggs <obriggs03@gmail.com>
2021-08-25 14:42:02 +0100

    Fixed get_user_type bug

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
     path('create_user_type/<int:uid>', views.create_user_type),
-    path('get_user_type/<int:uid>', views.UserTypeInfo.as_view(), name='view_user_type'),
+    path('get_user_type/<int:user_id>', views.UserTypeInfo.as_view(), name='view_user_type'),
 ]
